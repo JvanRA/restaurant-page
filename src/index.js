f4faffb8794ead14aeaa1876dcf9b270b15a170b
JvanRA <anorajivan@gmail.com>
2022-07-03 20:02:38 +0800

    Added navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World! :)")
+function createHeader() {
+    const header = document.createElement('header');
+    const restaurantName = document.createElement('h1');
+    const div = document.createElement('div');
+    const menu = document.createElement('p');
+    const contact = document.createElement('p');
+
+    restaurantName.textContent = 'Pizzeria';
+    menu.textContent = 'Menu';
+    contact.textContent = 'Contact';
+    header.appendChild(restaurantName);
+    header.appendChild(div);
+    div.appendChild(menu);
+    div.appendChild(contact);
+
+    return header;
+}
+
+function startWebsite() {
+    const content = document.getElementById('content');
+    content.append(createHeader());
+}
+
+startWebsite();
